1eafe808e9e781d32aceae73b819510c1c7f8058
Demian Petropavlov <demyanpain@gmail.com>
2025-03-18 10:43:09 +0400

    Добавление Secrets файла для ревью

---
+++ TravelSchedule/Config/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  TravelSchedule
+//
+//  Created by Demain Petropavlov on 13.03.2025.
+//
+
+import Foundation
+
+struct Secrets {
+    static let apiKey = "566536c3-b2cd-4950-99dc-8ae338024843"
+}
