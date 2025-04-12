24e8d86e45218f468f2124b3dc6d3e1113c775ef
Demian Petropavlov <demyanpain@gmail.com>
2025-04-12 18:10:40 +0400

    Добавлен TabBar

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -7,13 +7,39 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
+    @State private var selectedTab = 0
+    @State private var errorState: SettingsErrorState = .none
+
     var body: some View {
         NavigationView {
-            RouteInputView()
+            TabView(selection: $selectedTab) {
+                
+                // Первая вкладка — маршрут
+                RouteInputView()
+                    .tabItem {
+                        Image("schedule_image")
+                            .renderingMode(.template)
+                            .foregroundColor(selectedTab == 0 ? .black : .gray)
+                    }
+                    .tag(0)
+
+                // Вторая вкладка — настройки
+                SettingsView(errorState: $errorState)
+                    .tabItem {
+                        Image("settings_image")
+                            .renderingMode(.template)
+                            .foregroundColor(selectedTab == 1 ? .black : .gray)
+                    }
+                    .tag(1)
+            }
+            .tint(.black) // Устанавливаем основной цвет для активных элементов TabBar
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
